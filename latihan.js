5b79fbf2aa96ef5f8e1706125be9f15cb6544a6d
Mamat1411 <muhammadmujahid14@gmail.com>
2021-11-29 16:26:00 +0700

    Trying Simple Logic with Popup Box in JavaScript

--- latihan.js
+++ latihan.js
@@ -1,2 +1,17 @@
-var name = prompt('Masukkan Nama: ');
-alert(name);
+do {
+    var name = prompt('Masukkan Nama: ');
+    var sure = confirm('Yakin namanya ' + name + '?');
+    if (sure === true) {
+        alert('Hello '+ name);
+    }
+} while (sure != true);
+// var name = prompt('Masukkan Nama: ');
+// var sure = confirm('Yakin namanya ' + name + '?');
+// if (sure === true) {
+//     alert('Hello '+ name);
+// } else if (sure === false) {
+//     while (sure != true) {
+//         var name = prompt('Masukkan Nama: ');
+//         var sure = confirm('Yakin namanya ' + name + '?');
+//     }
+// }
