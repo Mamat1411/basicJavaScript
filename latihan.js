b90e6c3cd55a3d4b7c0214612c8b2085aec69fbc
Mamat1411 <muhammadmujahid14@gmail.com>
2021-11-30 14:58:53 +0700

    Trying to Make Star Patterns

--- latihan.js
+++ latihan.js
@@ -1,17 +1,16 @@
-do {
-    var name = prompt('Masukkan Nama: ');
-    var sure = confirm('Yakin namanya ' + name + '?');
-    if (sure === true) {
-        alert('Hello '+ name);
-    }
-} while (sure != true);
-// var name = prompt('Masukkan Nama: ');
-// var sure = confirm('Yakin namanya ' + name + '?');
-// if (sure === true) {
-//     alert('Hello '+ name);
-// } else if (sure === false) {
-//     while (sure != true) {
-//         var name = prompt('Masukkan Nama: ');
-//         var sure = confirm('Yakin namanya ' + name + '?');
+// do {
+//     var name = prompt('Masukkan Nama: ');
+//     var sure = confirm('Yakin namanya ' + name + '?');
+//     if (sure === true) {
+//         alert('Hello '+ name);
 //     }
-// }
+// } while (sure != true);
+var star = '';
+var n = prompt('How Many Rows and Column Do You Want To Print?')
+for (let i = n; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log(star);
