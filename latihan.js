515c78f882659a9eb7316080333a38f674506c4f
Mamat1411 <muhammadmujahid14@gmail.com>
2021-12-13 13:17:56 +0700

    Making a simple game called "Suwit Jawa"

--- latihan.js
+++ latihan.js
@@ -1,16 +1,25 @@
-// do {
-//     var name = prompt('Masukkan Nama: ');
-//     var sure = confirm('Yakin namanya ' + name + '?');
-//     if (sure === true) {
-//         alert('Hello '+ name);
-//     }
-// } while (sure != true);
-var star = '';
-var n = prompt('How Many Rows and Column Do You Want To Print?')
-for (let i = n; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-        star += '*';
+do {
+    var player = prompt ("Pilih Gajah, Orang atau Semut?");
+
+    var comp = Math.floor(Math.random() * 11);
+
+    if (comp < 10 && comp > 6) {
+        comp = "Gajah";
+    } else if (comp < 7 && comp > 3) {
+        comp = "Orang";
+    } else {
+        comp = "Semut";
     }
-    star += '\n';
-}
-console.log(star);
+
+    var hasil = "";
+    if (player == comp) {
+        hasil = "Seri";
+    } else if (player == "Gajah") {
+        hasil = (comp == "Orang") ? "Menang" : "Kalah";
+    } else if (player == "Semut") {
+        hasil = (comp == "Gajah") ? "Menang" : "Kalah";
+    }
+
+    alert("Kamu memilih : " + player + " dan komputer memilih: " + comp + "\nMaka hasilnya adalah: Kamu " + hasil +"!!!");
+    var repeat = confirm("Main lagi?");
+} while (repeat);
