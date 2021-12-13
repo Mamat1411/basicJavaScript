d1883e5f077aa5b0b387eeead98e463d73888fa7
Mamat1411 <muhammadmujahid14@gmail.com>
2021-12-13 15:08:29 +0700

    Adding conditions where users' number is out of range

--- latihan.js
+++ latihan.js
@@ -33,20 +33,24 @@ do {
         var number = randomizedNumber;
         alert("Guess a number between 1-10. \nYou have " + chances + " chance(s) remaining.");
         var guess = prompt("Pick a number from 1-10");
-        if (guess == number) {
-            alert("Great!! You guess it correctly!!");
-            break;
-        } else if (guess < number) {
-            alert("Your number is too low from the correct number!");
-            chances -= 1;
-        } else if (guess > number) {
-            alert("Your number is too high from the correct number!");
-            chances -= 1;
-        }
-        if (chances > 0) {
-            tryAgain = confirm("Try Again?");
+        if (guess > 0 && guess <= 10) {
+            if (guess == number) {
+                alert("Great!! You guess it correctly!!");
+                break;
+            } else if (guess < number) {
+                alert("Your number is too low from the correct number!");
+                chances -= 1;
+            } else if (guess > number) {
+                alert("Your number is too high from the correct number!");
+                chances -= 1;
+            }
+            if (chances > 0) {
+                tryAgain = confirm("Try Again?");
+            } else {
+                alert("You Lose! You run out of chances.");
+            }
         } else {
-            alert("You Lose! You run out of chances.");
+            alert("Your number is out of range.");
         }
     }
     var playAgain = confirm("Play Again?");
