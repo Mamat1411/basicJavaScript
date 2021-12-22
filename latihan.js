f5273f790b76f62c4cdede9c1bc9a00ce2e9e840
Mamat1411 <muhammadmujahid14@gmail.com>
2021-12-22 11:24:20 +0700

    Initiating Mini Project of Public Transport Passanger Management

--- latihan.js
+++ latihan.js
@@ -1,3 +1,4 @@
+// Game Suit
 // do {
 //     var player = prompt ("Pilih Gajah, Orang atau Semut?");
 
@@ -25,33 +26,59 @@
 //     alert("Kamu memilih : " + player + " dan komputer memilih: " + comp + "\nMaka hasilnya adalah: Kamu " + hasil +"!!!");
 //     var repeat = confirm("Main lagi?");
 // } while (repeat);
-do {
-    var chances = 3;
-    var tryAgain = true;
-    var randomizedNumber = 1 + Math.floor(Math.random() * 9);
-    while (chances > 0 && tryAgain) {
-        var number = randomizedNumber;
-        alert("Guess a number between 1-10. \nYou have " + chances + " chance(s) remaining.");
-        var guess = prompt("Pick a number from 1-10");
-        if (guess > 0 && guess <= 10) {
-            if (guess == number) {
-                alert("Great!! You guess it correctly!!");
-                break;
-            } else if (guess < number) {
-                alert("Your number is too low from the correct number!");
-                chances -= 1;
-            } else if (guess > number) {
-                alert("Your number is too high from the correct number!");
-                chances -= 1;
-            }
-            if (chances > 0) {
-                tryAgain = confirm("Try Again?");
-            } else {
-                alert("You Lose! You run out of chances.");
-            }
-        } else {
-            alert("Your number is out of range.");
+
+//Guess The Number Game
+// do {
+//     var chances = 3;
+//     var tryAgain = true;
+//     var randomizedNumber = 1 + Math.floor(Math.random() * 9);
+//     while (chances > 0 && tryAgain) {
+//         var number = randomizedNumber;
+//         alert("Guess a number between 1-10. \nYou have " + chances + " chance(s) remaining.");
+//         var guess = prompt("Pick a number from 1-10");
+//         if (guess > 0 && guess <= 10) {
+//             if (guess == number) {
+//                 alert("Great!! You guess it correctly!!");
+//                 break;
+//             } else if (guess < number) {
+//                 alert("Your number is too low from the correct number!");
+//                 chances -= 1;
+//             } else if (guess > number) {
+//                 alert("Your number is too high from the correct number!");
+//                 chances -= 1;
+//             }
+//             if (chances > 0) {
+//                 tryAgain = confirm("Try Again?");
+//             } else {
+//                 alert("You Lose! You run out of chances.");
+//             }
+//         } else {
+//             alert("Your number is out of range.");
+//         }
+//     }
+//     var playAgain = confirm("Play Again?");
+// } while (playAgain);
+
+//Public Transport Passanger Management
+var passanger = ["Mamat", undefined, "Mujahid"];
+function addPassanger(name, passanger) {
+    if (passanger.length == 0) {
+        passanger.push(name);
+        return passanger;
+    } else {
+        for (var i = 0; i < passanger.length; i++) {
+            if (passanger[i] == undefined) {
+                passanger[i] = name;
+                return passanger;
+            } 
+            if (name == passanger[i]) {
+                alert("This passanger already exists!");
+                return passanger;
+            } 
+            // if (passanger.length !== 0) {
+            //     passanger.push(name);
+            //     return passanger;
+            // }
         }
     }
-    var playAgain = confirm("Play Again?");
-} while (playAgain);
+}
